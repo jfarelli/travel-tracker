fa14e57278e15bfba18cb5ba6d0add8bbbffe8cb
Jordan Farelli <jordanfarelli@gmail.com>
2022-06-11 16:15:07 -0600

    Added additional tests to test functionality in repositories

--- test/DestinationsRepository-test.js
+++ test/DestinationsRepository-test.js
@@ -1,10 +1,10 @@
 import { expect } from 'chai';
-import DestinationsRepository from '../src/DestinationsRepository';
 import destinationsData from '../dist/sampleData/sampleData-Destinations';
+import DestinationsRepository from '../src/DestinationsRepository';
 
 describe('Destinations Repository', () => {
-    let destinations1;
-    let destinations2;
+    let destinations1 = [];
+    let destinations2 = [];
     beforeEach(() => {
         destinations1 = new DestinationsRepository(destinationsData);
         destinations2 = new DestinationsRepository(destinationsData);
@@ -12,8 +12,8 @@ describe('Destinations Repository', () => {
     it('should be a function', () => {
       expect(DestinationsRepository).to.be.a('function');
     });
-    it('should be able to get Travelers data by id', () => {
-        expect(destinations1.getDestinations(1)).to.deep.equal({
+    it('should be able to get Destination data by id', () => {
+        expect(destinations1.getDestinationsbyId(1)).to.deep.equal({
             "id": 1,
             "destination": "Lima, Peru",
             "estimatedLodgingCostPerDay": 70,
@@ -21,7 +21,7 @@ describe('Destinations Repository', () => {
             "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
             "alt": "overview of city buildings with a clear sky"
             });
-        expect(destinations2.getDestinations(2)).to.deep.equal({
+        expect(destinations2.getDestinationsbyId(2)).to.deep.equal({
             "id": 2,
             "destination": "Stockholm, Sweden",
             "estimatedLodgingCostPerDay": 100,
@@ -29,5 +29,9 @@ describe('Destinations Repository', () => {
             "image": "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
             "alt": "city with boats on the water during the day time"
             });
+    });
+    it('should calculate total trip cost', () => {
+       expect(destinations1.getTripCostTotal(1, 1, 8)).to.equal(1056.00);
+       expect(destinations2.getTripCostTotal(2, 3, 4)).to.equal(3014.00);
     });
 });
