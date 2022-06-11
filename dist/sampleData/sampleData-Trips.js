1264492caef808c4e4a928277e5d2453152b0257
Jordan Farelli <jordanfarelli@gmail.com>
2022-06-11 16:14:30 -0600

    Added additional sample data to each file

--- dist/sampleData/sampleData-Trips.js
+++ dist/sampleData/sampleData-Trips.js
@@ -1,8 +1,8 @@
 const tripsData = [
     {
     "id": 1,
-    "userID": 44,
-    "destinationID": 49,
+    "userID": 1,
+    "destinationID": 1,
     "travelers": 1,
     "date": "2022/09/16",
     "duration": 8,
@@ -11,11 +11,51 @@ const tripsData = [
     },
     {
     "id": 2,
-    "userID": 35,
-    "destinationID": 25,
+    "userID": 1,
+    "destinationID": 2,
+    "travelers": 3,
+    "date": "2019/08/30",
+    "duration": 4,
+    "status": "approved",
+    "suggestedActivities": []
+    },
+    {
+    "id": 3,
+    "userID": 1,
+    "destinationID": 3,
+    "travelers": 1,
+    "date": "2020/07/17",
+    "duration": 5,
+    "status": "approved",
+    "suggestedActivities": []
+    },
+    {
+    "id": 4,
+    "userID": 2,
+    "destinationID": 4,
     "travelers": 5,
     "date": "2022/10/04",
     "duration": 18,
+    "status": "approved",
+    "suggestedActivities": []
+    },
+    {
+    "id": 5,
+    "userID": 2,
+    "destinationID": 5,
+    "travelers": 3,
+    "date": "2021/06/29",
+    "duration": 9,
+    "status": "approved",
+    "suggestedActivities": []
+    },
+    {
+    "id": 6,
+    "userID": 2,
+    "destinationID": 6,
+    "travelers": 5,
+    "date": "2022/5/28",
+    "duration": 20,
     "status": "approved",
     "suggestedActivities": []
     }
