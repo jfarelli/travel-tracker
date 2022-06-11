6132e78cdf23d36cc33957627acf6ac1c156e04f
Jordan Farelli <jordanfarelli@gmail.com>
2022-06-11 17:21:54 -0600

    Created getRandomUser & displayTripsOnDashboard that shows all trips for user

--- src/scripts.js
+++ src/scripts.js
@@ -1,27 +1,24 @@
 import './css/styles.css';
 import './images/turing-logo.png'
 import {getData} from './apiCalls';
-
-
 import TripsRepository from './TripsRepository';
+import Travelers from './Travelers';
 import TravelersRepository from './TravelersRepository';
 import DestinationsRepository from './DestinationsRepository';
-
+import dayjs from 'dayjs';
+import Trips from './Trips';
+dayjs().format();
 // const dayjs = require('dayjs')
 // dayjs().format()
-import dayjs from 'dayjs';
-dayjs().format();
 
 
 // >>>>>> Gloval Variables <<<<<<
-var tripsData;
-var travelersData;
-var destinationsData;
-
+var tripsData = [];
+var travelersData = [];
+var destinationsData = [];
 var tripsRepository;
 var travelersRepository;
 var destinationsRepository;
-
 
 // >>>>>> Event Listenerts <<<<<<
 window.addEventListener('load', loadData);
@@ -37,5 +34,18 @@ function loadData() {
         tripsRepository = new TripsRepository( tripsData );
         travelersRepository = new TravelersRepository( travelersData );
         destinationsRepository = new DestinationsRepository( destinationsData );
+        console.log(displayTripsOnDashboard())
     });
+}
+
+function getRandomUser( traveler ) {
+    const randomUserIndex = Math.floor( Math.random() * traveler.length )
+    console.log(traveler[ randomUserIndex ])
+    return traveler[ randomUserIndex ]
+}
+  
+  function displayTripsOnDashboard() {
+    let randomUser = getRandomUser(travelersData);
+    let showUserTrips = new TripsRepository(tripsData);
+    return showUserTrips.getTripsByUserId(randomUser.id)
 }
